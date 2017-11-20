0fac3576c94de8afd03fc53dc8f4d675b896c425
federico <federico@federico-PC>
2017-11-19 23:25:24 -0300

    Tiempo empezado

--- src/Tiempo.java
+++ src/Tiempo.java
@@ -1,3 +1,7 @@
+
+import java.io.File;
+import java.util.Arrays;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,7 +12,28 @@
  *
  * @author federico
  */
-public class Tiempo
+public class Tiempo 
 {
+    private int[][] alfa_beta;
+    private long[] timestamps;
     
+    public Tiempo ()
+    {
+        alfa_beta = new int[2][20];
+        timestamps = new long[20];
+        Arrays.fill(alfa_beta[1], 10000);
+        for(int i=0; i < 20; i++)
+            System.out.println(alfa_beta[1][i]);
+        
+        StringBuffer buff = new StringBuffer();
+        String filePath = new File("").getAbsolutePath();
+        String path = "/src/datos/log_piezas.txt";
+        buff.append(filePath);
+        buff.append(path);
+        path = buff.toString();
+        buff.delete(filePath.length(), buff.length());
+        
+        alfa_beta[0][3] = 30;
+        alfa_beta[0][4] = 24;
+    }
 }
